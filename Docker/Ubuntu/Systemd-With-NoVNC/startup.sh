91bf240e046bfa7dd5fc9a6aff737b0ee2eb27b3
JohnB17 <63297273+JohnB17@users.noreply.github.com>
2022-10-26 20:40:02 -0400

    Update startup.sh

--- Docker/Ubuntu/Systemd-With-NoVNC/startup.sh
+++ Docker/Ubuntu/Systemd-With-NoVNC/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /lib/systemd/systemd
 /usr/bin/vncserver -depth 24 -geometry $RESOLUTION  :%i
-/var/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 6900
+/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 6900
